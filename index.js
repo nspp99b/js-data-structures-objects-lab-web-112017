a9be46cbd0eed41bae7c781b57dbc899863add7b
nspp99b <chromepanthers@gmail.com>
2018-01-08 15:46:49 -0500

    defined functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  o = Object.assign({}, driver)
+  o[key] = value
+  return o
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+  newdriver = Object.assign({}, driver)
+  return delete newdriver[key]
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  return delete driver[key]
+}
